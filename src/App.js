60e4c1f0d84826cff73486db9d48a13425c409bc
Diwash <diwashdahal75@gmail.com>
2021-12-08 22:18:39 +0545

    add textutils simple dark mode and bold copy clear features

--- src/App.js
+++ src/App.js
@@ -1,28 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand" href="/">TextUtils</a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-  <div className="collapse navbar-collapse" id="navbarNav">
-    <ul className="navbar-nav">
-      <li className="nav-item">
-        <a className="nav-link" href="/">Home</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="/">About</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link disabled" href="/" tabIndex="-1" aria-disabled="true">Disabled</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+    <Navbar title="TextUtils" />
+    <TextForm heading="Enter the text to analyze"/>
     </>
   );
 }
---
+++ src/components/TextForm.js
@@ -0,0 +1,89 @@
+import React, {useState} from 'react'
+
+export default function TextForm(props) {
+
+    const handleUp = () => {
+        setText(text.toUpperCase());
+    }
+    const handleLow = () => {
+        setText(text.toLowerCase());
+    }
+
+    const handleChange = (event) => {
+        setText(event.target.value);
+    }
+
+    const handleClear = () => {
+        setText("");
+    }
+
+    const handleBold = () => {
+        if(!(text.indexOf("<b>") > -1 || text.indexOf("<B>") > -1))
+            setText(text.bold());
+    }
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText(text);
+    }
+
+    const toggleStyle = () => {
+        if(myStyle.color === "black"){
+            setStyle({
+                'color': 'white',
+                'background': 'black'
+            });
+            setBtnText("Light mode");
+        }
+        else {
+            setStyle({
+                'color': 'black',
+                'background': 'white'
+            });
+            setBtnText("Dark mode");
+        }
+    }
+
+    const getWords = (text) => {
+        let words = text.replace(/\s{2,}/g,' ').split(/\s|\n/);
+        for(let i = 0; i < words.length; i++) {
+            if(words[i] === '') words.splice(i, 1);
+        }
+        return words.length
+    }
+
+    const [myStyle, setStyle] = useState({
+        'color': 'black',
+        'background': 'white'
+    })
+    const [btnText, setBtnText] = useState("Dark mode")
+    const [text, setText] = useState('');
+    return (
+        <>
+        <div className="container" style={myStyle}>
+            <form>
+            <div className="form-group mt-5">
+            <h2>{props.heading}</h2>
+            <textarea className="form-control" onChange={handleChange} value={text} id="txt" rows="8"></textarea>
+            </div>
+            </form>
+            <em>Result:</em>
+            <div className="container border p-2 mb-3">
+                <p className="display"  dangerouslySetInnerHTML={{__html: text}}></p>
+            </div>
+            <button className="btn btn-dark" onClick={handleBold}>Bold</button>
+            <button className="btn btn-primary ml-3" onClick={handleUp}>Uppercase</button>
+            <button className="btn btn-primary ml-3" onClick={handleLow}>Lowercase</button>
+            <button className="btn btn-danger ml-3" onClick={handleClear}>Clear</button>
+            <button className="btn btn-secondary ml-3" onClick={handleCopy}>Copy</button>
+        </div>
+        <div className="container mt-5">
+            <h3>Your text summary:</h3>
+            <p> { getWords(text) } words <br/>
+                { text.length } characters <br/>
+                { getWords(text) * 0.008 } minutes read
+            </p>
+        </div>
+        <button className="btn btn-dark mr-3 position-sticky float-right" onClick={toggleStyle}>{btnText}</button>
+        </>
+    )
+}
